f7c7d636560e4050dadb16ae632cb552cd9168e8
Emmanuel López Guerrero <manelpz@icloud.com>
2020-09-03 00:36:14 -0500

    adding gitignore

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 // Global app controller
 
 import num from './test';
-
-console.log(`import ${num} from module called test.js`);
+const  x = 23;
+console.log(`import ${num} from module called test.js. Variable ${x}`);
