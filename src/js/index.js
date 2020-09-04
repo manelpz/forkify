8224c841acd4ee94db73b1f49b01cf584976056b
Emmanuel López Guerrero <manelpz@icloud.com>
2020-09-04 00:31:30 -0500

    Adding api call

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
 async function getResults(query){
+    const proxy = 'https://api.allorigins.win/get?url=';
+    
+    try{
     const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-    //const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=pizza`);
-    console.log(res);
+    const recipies = res.data.recipes;
+    console.log(recipies);
+    }catch(error){
+        alert(error);
+    }
 }
 getResults('pizza');
