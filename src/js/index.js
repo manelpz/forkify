7f085f5407453e07fa131ea6c69b475174d9704c
Emmanuel López Guerrero <manelpz@icloud.com>
2020-09-09 23:10:33 -0500

    adding search functionality

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,16 @@ import Search from './models/Search';
 /*global state of the app*/
 const state = {};
 
-const controlSearch = () => {
+const controlSearch = async() => {
     const query = 'pizza'
 
     if (query){
+        // new search object 
         state.search = new Search(query);
-    }       
+
+        await state.search.getResults();
+        console.log(state.search.result);
+    }   
 }
 
 document.querySelector('.search').addEventListener('submit', e=>{
@@ -18,5 +22,5 @@ document.querySelector('.search').addEventListener('submit', e=>{
 
 
 
-search.getResults();
+
 
