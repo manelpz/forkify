e1825548ac53099c959d47301ea1997ce9b2cf4c
Emmanuel López Guerrero <manelpz@icloud.com>
2020-09-09 23:02:59 -0500

    adding search

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,20 @@
 import Search from './models/Search';
 
+/*global state of the app*/
+const state = {};
+
+const controlSearch = () => {
+    const query = 'pizza'
+}
+
+document.querySelector('.search').addEventListener('submit', e=>{
+    e.preventDefault();
+    controlSearch();
+});
+
+
 const search = new Search('pizza');
 console.log(search);
 
 search.getResults();
+
