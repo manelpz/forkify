c96dea46b27190bc53754895f19efbdfce679189
Emmanuel López Guerrero <manelpz@icloud.com>
2020-09-13 01:17:21 -0500

    adding classes

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,6 @@
 
 export const elements = {
     searchForm: document.querySelector('.search'),
-    searchInput: document.querySelector('.search__field')
+    searchInput: document.querySelector('.search__field'),
+    searchResult: document.querySelector('.results_list')
 };
