9a81b1b53305cac74e626f2a6c26e49556e15f92
Emmanuel López Guerrero <manelpz@icloud.com>
2020-09-15 23:49:18 -0500

    clear elements

--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -1,6 +1,9 @@
 
 import {elements} from './base';
 export const getInput= () => elements.searchInput.value;
+export const clearInput = () => {
+    elements.searchInput.value ='';
+}
 
 const renderRecipe =  recipe => {
     const markup = `
