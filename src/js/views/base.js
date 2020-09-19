b2bcdcf9b41abe0464071fd1c397bc1d8220131a
Emmanuel López Guerrero <manelpz@icloud.com>
2020-09-18 22:25:09 -0500

    adding loader

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -6,12 +6,20 @@ export const elements = {
     searchResList: document.querySelector('.results__list')
 };
 
+export const elementsStrings = {
+    loader: 'loader'
+};
+
 export const renderLoader = parent => {
     const loader = `
-    <div class="loader">
+    <div class="${elements.loader}">
         <svg>
             <use href="img/icons.svg#icon-cw"></use>
         </svg>
     </div>`;
     parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+    const loader = document.querySelector(elements.loader);
 };
