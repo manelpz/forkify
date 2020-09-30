9813702bf7b4f6c2272b6fa7098108f2a912079e
Emmanuel López Guerrero <manelpz@icloud.com>
2020-09-29 22:36:59 -0500

    adding eval

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -48,14 +48,23 @@ export default class Recipe {
             const arrIng = ingredient.split(' ');
             const unitIndex = arrIng.findIndex(el2 => unitShort.includes(el2));
 
-            
+            let objIng;
             if(unitIndex >-1){
                 const arrCount = arrIng.slice(0, unitIndex);
 
                 let count;
                 if(arrCount.length === 1){
-                    count = arrIng[0];
+                    count = eval(arrIng[0].replace('-','+'));
+                }else{
+                    count = eval(arrIng.slice(0, unitIndex).join('+'));
                 }
+
+                objIng = {
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex+1).join(' ')
+                };
+
             }else if (parseInt(arrIng[0],10)){
                 objIng = {
                     count: parseInt(arrIng[0], 10),
